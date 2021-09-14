d3e7f5369e6ddbbbac90e16ff39e297bc89ae1f6
Vanilla <neko@hydev.org>
2021-09-14 18:17:49 +0800

    [+] SimpleEchoServer.kt: Show the message to UI.

--- app/src/main/java/github/vanilla/socketplayer/SimpleEchoServer.kt
+++ app/src/main/java/github/vanilla/socketplayer/SimpleEchoServer.kt
@@ -1,6 +1,5 @@
 package github.vanilla.socketplayer
 
-import android.util.Log
 import github.vanilla.socketplayer.databinding.ActivityMainBinding
 import io.ktor.network.selector.*
 import io.ktor.network.sockets.*
@@ -21,23 +20,17 @@ object SimpleEchoServer {
 
         val server = aSocket(ActorSelectorManager(Dispatchers.IO)).tcp()
             .bind(InetSocketAddress("0.0.0.0", 2323))
-        Log.d("TAG", "Started echo telnet server at ${server.localAddress}")
         serverInit = true
 
         while (true) {
             val socket = server.accept()
+            val input = socket.openReadChannel()
 
             launch {
-                Log.d("TAG", "Socket accepted: ${socket.remoteAddress}")
-
-                val input = socket.openReadChannel()
-                val output = socket.openWriteChannel(autoFlush = true)
-
                 try {
                     while (true) {
                         val line = input.readUTF8Line()
-                        output.writeStringUtf8("$line\r\n")
-                        // activity.runOnUiThread { binding.networkInfo.text = line }
+                        activity.runOnUiThread { binding.networkInfo.text = line }
                     }
                 } catch (ignored: Throwable) {
                 }
