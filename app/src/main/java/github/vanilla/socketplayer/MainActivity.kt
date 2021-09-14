05ff99fddc2103888983be3f6643f2ab18914709
Vanilla <neko@hydev.org>
2021-09-14 15:34:56 +0800

    [+] Simple program to show ip address, with using view-binding.

--- app/src/main/java/github/vanilla/socketplayer/MainActivity.kt
+++ app/src/main/java/github/vanilla/socketplayer/MainActivity.kt
@@ -1,11 +1,48 @@
 package github.vanilla.socketplayer
 
+import android.annotation.SuppressLint
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.Network
+import android.net.wifi.WifiManager
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.common.primitives.Ints
+import github.vanilla.socketplayer.databinding.ActivityMainBinding
+import java.net.InetAddress
 
 class MainActivity : AppCompatActivity() {
+    // https://developer.android.com/topic/libraries/view-binding#activities
+    private lateinit var binding: ActivityMainBinding
+
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // https://developer.android.com/training/basics/network-ops/reading-network-state#instantaneous
+        val connectivityManager = getSystemService(ConnectivityManager::class.java)
+        // https://developer.android.com/training/basics/network-ops/reading-network-state#listening-events
+        connectivityManager.registerDefaultNetworkCallback(object :
+            ConnectivityManager.NetworkCallback() {
+            override fun onAvailable(network: Network) {
+                val wm = getSystemService(Context.WIFI_SERVICE) as WifiManager
+
+                // https://stackoverflow.com/questions/1957637/java-convert-int-to-inetaddress
+                val bytes = Ints.toByteArray(wm.connectionInfo.ipAddress)
+                val address = InetAddress.getByAddress(bytes)
+
+                // https://developer.android.com/training/monitoring-device-state/connectivity-status-type
+                val cm = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+                if (cm.isActiveNetworkMetered) return
+
+                runOnUiThread { binding.networkInfo.text = "Ip Address: ${address.hostAddress}" }
+            }
+
+            override fun onLost(network: Network) {
+                runOnUiThread { binding.networkInfo.text = "Wifi connection lost. " }
+            }
+        })
     }
 }
