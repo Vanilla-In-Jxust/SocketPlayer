3fb0ff118bdcaaa00ce67640a566329cf056b363
Vanilla <neko@hydev.org>
2021-09-17 09:52:05 +0800

    [F] Fix infinite loop in socket main loop.

--- app/src/main/java/github/vanilla/socketplayer/ControlMediaServer.kt
+++ app/src/main/java/github/vanilla/socketplayer/ControlMediaServer.kt
@@ -6,10 +6,7 @@ import github.vanilla.socketplayer.activities.PlayerActivity
 import io.ktor.network.selector.*
 import io.ktor.network.sockets.*
 import io.ktor.utils.io.*
-import kotlinx.coroutines.DelicateCoroutinesApi
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.*
 import java.net.InetSocketAddress
 
 // https://ktor.io/docs/servers-raw-sockets.html#client
@@ -26,12 +23,17 @@ object ControlMediaServer {
 
         while (true) {
             val socket = server.accept()
-            val input = socket.openReadChannel()
 
             launch {
+                val input = socket.openReadChannel()
+
                 try {
                     while (true) {
                         val line = input.readUTF8Line()
+                        if (line == null) { // line == null means some socket disconnected.
+                            delay(1000L)
+                            continue
+                        }
 
                         // https://developer.android.com/training/basics/firstapp/starting-activity#BuildIntent
                         Intent(activity, PlayerActivity::class.java).apply {
